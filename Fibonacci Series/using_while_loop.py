cbc080056e4a95ee512186edd90af22d59c7afff
NealKumawat <nealkumawat04@gmail.com>
2026-02-27 18:36:16 +0000

    updated

--- Fibonacci Series/using_while_loop.py
+++ Fibonacci Series/using_while_loop.py
@@ -1,6 +1,11 @@
 n=int(input("Give a number till you want the Fibonacci: "))
 l=[0,  1]
-while i<=n:     # i could be used as index
-    i=2
-    
-    l.append()
+i = 1
+while i<n-1:     # i could be used as index
+    s = l[i] + l[i-1]
+    if i==1:
+        i==2
+    l.append(s)
+    i=i+1
+
+print(l)
