11f44632e4f25f46b57905f5da0a6429f7109be8
NealKumawat <nealkumawat04@gmail.com>
2026-03-02 19:44:58 +0000

    updated

--- Fibonacci Series/using_while_loop.py
+++ Fibonacci Series/using_while_loop.py
@@ -1,14 +1,14 @@
 n=int(input("Give a number till you want the Fibonacci: "))
 l=[0,  1]
 i = 1
-while i<n-1:     # i could be used as index
-    s = l[i] + l[i-1]
-    if i==1:
-        i==2
-    l.append(s)
-    i=i+1
+if n<0:
+    print("Invalid Input")
+else:
+    while i<n-1:     # i could be used as index
+        s = l[i] + l[i-1]
+        if i==1:
+            i==2
+        l.append(s)
+        i=i+1
 
-print(l)
-
-
-#work on the negatives
+    print(l)
