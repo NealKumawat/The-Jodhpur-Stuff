ce02dc75c930862e7ddd596baf2a93b9cf9c7539
NealKumawat <nealkumawat04@gmail.com>
2026-03-06 14:48:11 +0000

    updated

--- Sum till Billion/sum.py
+++ Sum till Billion/sum.py
@@ -1,3 +1,10 @@
 """
 Here the most optimal soln is also solved in few hours
-There are approaches you need to know they are
+There are approaches you need to know they are:
+                    make your calculations short by not going to every number but just root of billion
+                    make the loop to go skipping 2s(evens)
+                    don't include 1 and the last number you want to check the primability
+                    don't calculate from start to billion in a single chunk, divide it into 10 maybe
+
+
+"""
